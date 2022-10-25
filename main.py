dab31d2462be8481978ad06268bb1e2ec1e57ba9
wepeqoor1 <hoopeeeee@gmail.com>
2022-10-26 00:12:42 +0300

    Add upload photo to server (must be fix)

--- main.py
+++ main.py
@@ -4,28 +4,15 @@ import requests
 from dotenv import load_dotenv
 
 from vk_api import (
-    get_address_for_download_photo,
+    get_address_for_upload_photo,
     upload_photo_to_server,
-    save_photo_in_albom_group,
+    save_photo_in_album_group,
     publish_in_wall_group
 )
+from xkcd_api import download_comic_img, get_comic_data
 
 VERSION_API = 5.131
-
-
-def get_comic_data(comic_number: int) -> dict:
-    url = f"https://xkcd.com/{comic_number}/info.0.json"
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
-
-
-def download_comic_img(url: str) -> None:
-    file_name = 'comic.png'
-    response = requests.get(url)
-    response.raise_for_status()
-    with open(file_name, 'wb') as file:
-        file.write(response.content)
+COMIC_IMG_NAME = 'comic.png'
 
 
 def main():
@@ -36,12 +23,19 @@ def main():
     comic_img = comics_description['img']
     download_comic_img(url=comic_img)
     # comic_comment = comics_description['alt']
-    address_photo = get_address_for_download_photo(
+    upload_url_photo = get_address_for_upload_photo(
         vk_access_token=vk_access_token,
         vk_group_id=vk_group_id,
         version_api=VERSION_API
     )
-    print(address_photo)
+    print(upload_url_photo)
+    data_from_upload_photo = upload_photo_to_server(
+        vk_access_token=vk_access_token,
+        vk_group_id=vk_group_id,
+        version_api=VERSION_API,
+        img_name=COMIC_IMG_NAME
+    )
+    print(data_from_upload_photo)
 
 
 if __name__ == '__main__':
--- vk_api.py
+++ vk_api.py
@@ -1,5 +1,7 @@
 import requests
 
+from utils import get_comic_img
+
 
 def get_address_for_upload_photo(vk_access_token: str, vk_group_id: str, version_api: float) -> dict:
     """Получает адресс для загрузки фото"""
@@ -11,17 +13,30 @@ def get_address_for_upload_photo(vk_access_token: str, vk_group_id: str, version
         'v': version_api
     }
     response = requests.get(url, params=params)
-    print(response.url)
     response.raise_for_status()
     return response.json()
 
 
-def upload_photo_to_server():
+def upload_photo_to_server(vk_access_token: str, vk_group_id: str, version_api: float, img_name: str) -> dict:
     """Загружает фото на сервер"""
-    pass
+    method = 'photos.saveWallPhoto'
+    url = f'https://api.vk.com/method/{method}/'
+    params = {
+        'access_token': vk_access_token,
+        'group_id': vk_group_id,
+        'v': version_api
+    }
+
+    with open(img_name, 'rb') as image:
+        files = {
+            'photo': image
+        }
+    response = requests.post(url, params=params, files=files)
+    response.raise_for_status()
+    return response.json()
 
 
-def save_photo_in_albom_group():
+def save_photo_in_album_group():
     """Сохраняет фото в альбоме группы"""
     pass
 
