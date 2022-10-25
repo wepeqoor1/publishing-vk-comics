1493ac8ed3be0e5c7c5bf93ab655a6717a4e0289
wepeqoor1 <hoopeeeee@gmail.com>
2022-10-26 00:10:50 +0300

    Move xkcd_requests ti file

---
+++ xkcd_api.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def get_comic_data(comic_number: int) -> dict:
+    url = f"https://xkcd.com/{comic_number}/info.0.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def download_comic_img(url: str) -> None:
+    file_name = 'comic.png'
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(file_name, 'wb') as file:
+        file.write(response.content)
