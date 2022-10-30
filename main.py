cc84371f11c815044e5b54698c78d8efc293b431
wepeqoor1 <hoopeeeee@gmail.com>
2022-10-30 20:18:07 +0300

    Split func for get comics data

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import random
 
 from dotenv import load_dotenv
 
@@ -9,7 +8,7 @@ from vk_api import (
     save_photo_in_album_group,
     publish_comic_on_wall
 )
-from xkcd_api import download_comic_img, get_comic_data, get_count_comics
+from xkcd_api import download_comic_img, get_comic_data
 
 COMIC_IMG_NAME = 'comic.png'
 
@@ -18,12 +17,8 @@ def main():
     vk_access_token = os.getenv('VK_ACCESS_TOKEN')
     vk_group_id = os.getenv('VK_GROUP_ID')
 
-    count_comics = get_count_comics()
-    random_comic = random.randint(1, count_comics)
-
-    comics_description = get_comic_data(comic_number=random_comic)
-    comic_img, comic_comment = comics_description['img'], comics_description['alt']
-    download_comic_img(url=comic_img)
+    comic_img_url, comic_comment = get_comic_data()
+    download_comic_img(url=comic_img_url)
 
     upload_url = get_address_for_upload_photo(
         vk_access_token=vk_access_token,
@@ -53,6 +48,7 @@ def main():
     )
 
     os.remove(COMIC_IMG_NAME)
+    print('Комикс опубликован на стене')
 
 
 if __name__ == '__main__':
--- xkcd_api.py
+++ xkcd_api.py
@@ -1,18 +1,27 @@
+from typing import NamedTuple
+
 import requests
 
 
-def get_count_comics() -> int:
-    url = f"https://xkcd.com/info.0.json"
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()['num']
+class ComicData(NamedTuple):
+    img_url: str
+    comment: str
 
 
-def get_comic_data(comic_number: int) -> dict:
-    url = f"https://xkcd.com/{comic_number}/info.0.json"
+def get_comic_data() -> ComicData:
+    # Получаем номер последнего комикса
+    last_comic_url = f"https://xkcd.com/info.0.json"
+    response = requests.get(last_comic_url)
+    response.raise_for_status()
+    last_comic = response.json()['num']
+
+    # Получаем данные по конкретному комиксу
+    url = f"https://xkcd.com/{last_comic}/info.0.json"
     response = requests.get(url)
     response.raise_for_status()
-    return response.json()
+    comic = response.json()
+
+    return ComicData(img_url=comic['img'], comment=comic['alt'])
 
 
 def download_comic_img(url: str) -> None:
